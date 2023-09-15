76e8ac0d1f9a36b66240f00e7de946eae805413f
MichaelJPuccini <michaeljpuccini@outlook.com>
2023-09-16 00:51:20 +1000

    Question 1 solution

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,25 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
 years = []
 co2 = []
 temp = []
+
+connection = sqlite3.connect("climate.db")
+cursor = connection.cursor()
+
+# Year, CO2, Temperature
+sql_command = "SELECT Year FROM ClimateData;"
+cursor.execute(sql_command)
+years = cursor.fetchall()
+
+sql_command = "SELECT CO2 FROM ClimateData;"
+cursor.execute(sql_command)
+co2 = cursor.fetchall()
+
+sql_command = "SELECT Temperature FROM ClimateData;"
+cursor.execute(sql_command)
+temp = cursor.fetchall()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -16,3 +33,5 @@ plt.ylabel("Temp (C)")
 plt.xlabel("Year (decade)") 
 plt.show() 
 plt.savefig("co2_temp_1.png") 
+
+connection.close()
