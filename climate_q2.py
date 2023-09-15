3ddf902fe49f393a90d8199279c7adb9d8de57c8
MichaelJPuccini <michaeljpuccini@outlook.com>
2023-09-16 01:24:36 +1000

    Question 2 solution

--- climate_q2.py
+++ climate_q2.py
@@ -1,9 +1,17 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import openpyxl
 
 years = []
 co2 = []
 temp = []
+
+wb = openpyxl.load_workbook('climate')
+sheet = wb.get_sheet_by_name('climate')
+
+years = sheet['A2':'A100']
+co2 = sheet['B2':'B100']
+temp = sheet['C2':'C100']
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
